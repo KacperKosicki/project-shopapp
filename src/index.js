9471298808eab6cca960f21190e9d22b277ada97
Kacper Kosicki <kosickikacper1@gmail.com>
2023-08-02 12:21:07 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import 'font-awesome/css/font-awesome.min.css';
+
+import './styles/normalize.scss';
+import './styles/global.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
