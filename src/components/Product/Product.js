3ec7d7d9207c7e3bdfa914dff8e0c07734900d77
Kacper Kosicki <kosickikacper1@gmail.com>
2023-08-04 12:05:13 +0200

    Use hook useMemo on project

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import styles from './Product.module.scss';
-import ProductImage from '../ProductImage/ProductImage';
-import ProductForm from '../ProductForm/ProductForm';
+import clsx from 'clsx';
+import Button from '../Button/Button';
 
 const Product = (props) => {
   const { name, title, basePrice, colors, sizes } = props;
@@ -12,41 +12,70 @@ const Product = (props) => {
     setSelectedColor(color);
   };
 
-  const handleSizeChange = (sizeName) => {
-    const selectedSizeObj = sizes.find((size) => size.name === sizeName);
-    setSelectedSize(selectedSizeObj);
+  const handleSizeChange = (size) => {
+    setSelectedSize(size);
   };
 
-  const getPrice = () => {
+  const getPrice = useMemo(() => {
     return basePrice + selectedSize.additionalPrice;
-  };
+  }, [basePrice, selectedSize]);
 
-  const handleAddToCart = () => {
-    console.log('Summary');
-    console.log('---==========---');
+  const handleAddToCart = (event) => {
+    event.preventDefault();
+    console.log('Product added to cart:');
     console.log('Name:', title);
-    console.log('Price:', getPrice());
+    console.log('Price:', getPrice);
     console.log('Color:', selectedColor);
     console.log('Size:', selectedSize);
   };
 
+  const imageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${selectedColor}.jpg`;
+
   return (
     <article className={styles.product}>
-        <ProductImage name={name} title={title} selectedColor={selectedColor} />
+      <div className={styles.imageContainer}>
+        <img className={styles.image} alt={title} src={imageUrl} />
+      </div>
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
-        <ProductForm
-          colors={colors}
-          selectedColor={selectedColor}
-          handleColorChange={handleColorChange}
-          sizes={sizes}
-          selectedSize={selectedSize}
-          handleSizeChange={handleSizeChange}
-          handleAddToCart={handleAddToCart}
-        />
+        <div>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              {sizes.map((size) => (
+                <li key={size.name}>
+                  <button
+                    type="button"
+                    className={clsx(styles.sizeButton, size === selectedSize && styles.active)}
+                    onClick={() => handleSizeChange(size)}
+                  >
+                    {size.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              {colors.map((color) => (
+                <li key={color}>
+                  <button
+                    type="button"
+                    className={clsx(styles.colorButton, styles[`color${color.charAt(0).toUpperCase() + color.slice(1)}`], color === selectedColor && styles.active)}
+                    onClick={() => handleColorChange(color)}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Button className={styles.button} onClick={handleAddToCart}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </div>
       </div>
     </article>
   );
